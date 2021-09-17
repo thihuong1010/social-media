d1065cf6b8ec15d0042ea213a15f12ece4e4ff86
Huong Nguyen <huongnguyen10102@gmail.com>
2021-09-17 22:51:55 +0300

    set up done

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+// load modules
 const express = require('express');
 const exphbs = require('express-handlebars');
-const app = express();
+const mongoose = require('mongoose'); // bring mongoose 
+
+const keys = require('./config/keys'); // connect to MongoURI from external file
+
+const app = express(); // initialize app
 
 // set handlebars as view engine
 app.engine('handlebars', exphbs({
@@ -12,7 +17,15 @@ app.set('view engine', 'handlebars');
 // express static folder to css or activate img... bla
 app.use(express.static('public'));
 
-const port = 3000;
+// connect to remote database
+mongoose.connect(keys.MongoURI, {
+    useNewUrlParser: true
+})
+.then(() => {
+    console.log('Connected to Remote db...');
+});
+
+const port =process.env.PORT || 3000; // set port, deploy || local
 
 // visit home page, message sent, send() method to send somethings on the screen
 app.get('/', (req, res) => {
@@ -21,7 +34,7 @@ app.get('/', (req, res) => {
 
 // bring express module
 app.get('/about', (req, res) => {
-    res.send('About page !');
+    res.render('about');
 });
 
 app.listen(port, () => {
