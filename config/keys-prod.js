8ec26f51aee3ff0dd7c1890211bc8c1f717b65f1
Huong Nguyen <huongnguyen10102@gmail.com>
2021-10-04 08:20:54 +0300

    deployment

---
+++ config/keys-prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://tomapp:Tomapp123@onlinetomapp.iic40.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    GoogleClientID: '14590242538-735igbolrm9l5fku3g18dqiitrsf263b.apps.googleusercontent.com',
+    GoogleClientSecret: 'MRZak-OfPL8rcW-uqr7zclQg'
+}
