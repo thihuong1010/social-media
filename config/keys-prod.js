64b321e0080077124442e3539c943bd33d3caeb6
Huong Nguyen <huongnguyen10102@gmail.com>
2021-10-04 08:55:55 +0300

    config keys vars

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://tomapp:Tomapp123@onlinetomapp.iic40.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    GoogleClientID: '14590242538-735igbolrm9l5fku3g18dqiitrsf263b.apps.googleusercontent.com',
-    GoogleClientSecret: 'MRZak-OfPL8rcW-uqr7zclQg'
+    MongoURI: process.env.MONGO_URI,
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
